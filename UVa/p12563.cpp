5f7ccf5126fb6f165d98ef36766c671d0cf36d2c
Bill Li <Spacelessd@outlook.com>
2018-06-16 08:55:46 +0800

    再修正一遍。。。。。。。跨编辑器真蛋疼
    Former-commit-id: 45def06878a62ffc06f0febdb3669c9520fe26c2

--- UVa/p12563.cpp
+++ UVa/p12563.cpp
@@ -21,25 +21,25 @@ int main()
         }
         if(t>sum)//t大于所有歌的长度,直接输出歌数+1时间加上劲歌金曲
         {
-            printf("Case %d: %d %d\n",l,n+1,sum+JGJQ);
-            continue;
+			printf("Case %d: %d %d\n",l,n+1,sum+JGJQ);
+			continue;
         }
-        f[0]=0,t=t-1;//留1s给劲歌金曲
+		f[0]=0,t=t-1;//留1s给劲歌金曲
         for(int i=1;i<=t;i++)
 			f[i]=-1;//初始化01背包,同时去掉劲歌金曲这首歌
         
         for(int i=1;i<=n;i++)//01背包计算最多能唱的歌曲数
-            for(int j=t;j>=a[i];j--)
-                f[j]=max(f[j],f[j-a[i]]+1);
+			for(int j=t;j>=a[i];j--)
+				f[j]=max(f[j],f[j-a[i]]+1);
         
-        int ma=-1;//求最长能唱的时间,ma表示能唱的歌数(不含劲歌金曲)
-        for(int i=t;i>=0;i--)
+		int ma=-1;//求最长能唱的时间,ma表示能唱的歌数(不含劲歌金曲)
+		for(int i=t;i>=0;i--)
             if(f[i]>ma)
             {
 				ma=f[i];
 				sum=i;
             }
-        printf("Case %d: %d %d\n",l,ma+1,sum+JGJQ);
-    }
-    return 0;
+		printf("Case %d: %d %d\n",l,ma+1,sum+JGJQ);
+	}
+	return 0;
 }
