8df3c05be80b7dc049a6fd420af577dd0b52fe30
Spaceless <Spacelessd@outlook.com>
2020-02-09 00:19:26 +0800

    Auto Push
    Former-commit-id: 695022d07dcb860efe19b6a001007840b48e9660

---
+++ Luogu/p1216.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int r;
+int g[1003][1003];
+
+int main()
+{
+    cin>>r;
+    for(int i=1;i<=r;i++)
+        for(int j=1;j<=i;j++)
+            cin>>g[i][j];
+    for(int i=r;i>=1;i--)
+        for(int j=1;j<i;j++)
+    return 0;
+}
