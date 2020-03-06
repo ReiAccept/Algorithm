42b931e3a67c2fb8bd6c68f8a9ab47ea57f87501
Spaceless <Spacelessd@outlook.com>
2020-03-06 23:44:45 +0800

    Auto Push
    Former-commit-id: 658d468b988a122bcbbed3cddeb28ebb52a56311

--- Luogu/p1855.cpp
+++ Luogu/p1855.cpp
@@ -3,11 +3,18 @@
 
 using namespace std;
 
-int n,m,t;
+int n,M,T;
+int m[1003],t[1003];
+int f[1003][1003];
 
 int main()
 {
-    scanf("%d%d%d",&n,&m,&t);
-
+    scanf("%d%d%d",&n,&M,&T);
+    for(int i=1;i<=n;i++) scanf("%d%d",&m[i],&t[i]);
+    for(int i=1;i<=n;i++)
+        for(int j=M;j>=m[i];j--)
+            for(int k=T;k>=t[i];k--)
+                f[j][k]=max(f[j][k],f[j-m[i]][k-t[i]]+1);
+    cout<<f[M][T]<<endl;
     return 0;
 }
