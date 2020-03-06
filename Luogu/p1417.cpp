5be2521b716f9512b626cb4955d77794b8306518
Space <Spacelessd@outlook.com>
2020-03-07 03:00:51 +0800

    Auto Push
    Former-commit-id: d66f206fd2a7ee118d6735a349afa1a072d670de

--- Luogu/p1417.cpp
+++ Luogu/p1417.cpp
@@ -1,9 +1,33 @@
 #include<cstdio>
 #include<iostream>
+#include<algorithm>
 
 using namespace std;
 
+struct Node
+{
+    int a,b,c;
+}a[53];
+
+int T,n;
+long long ans;
+long long f[100003];
+
+bool cmp(Node x,Node y)
+{
+    return x.c*y.b<y.c*x.b;
+}
+
 int main()
 {
+    scanf("%d%d",&T,&n);
+    for(int i=1;i<=n;i++)scanf("%d",&a[i].a);
+    for(int i=1;i<=n;i++)scanf("%d",&a[i].b);
+    for(int i=1;i<=n;i++)scanf("%d",&a[i].c);
+    sort(a+1,a+1+n,cmp);
+    for(int i=1;i<=n;i++)
+        for(int j=T;j>=a[i].c;j--)
+            ans=max(ans,f[j]=max(f[j-a[i].c]+a[i].a-(long long)j*a[i].b,f[j]));
+    cout<<ans<<endl;
     return 0;
 }
