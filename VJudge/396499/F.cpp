c6ecd8ee0a499cb8087ef0d15905069577db6a64
Space <Spacelessd@outlook.com>
2020-09-23 16:43:38 +0800

    Auto Push
    Former-commit-id: 7acfa917e59e363a62769bca15080354ec95d4cc

--- VJudge/396499/F.cpp
+++ VJudge/396499/F.cpp
@@ -3,10 +3,10 @@
 #define ull unsigned long long
 using namespace std;
 int qrd(){int a=0,b=1,c=getchar();while(c>'9'||c<'0'){if(c=='-')b=-1;c=getchar();}while(c>='0'&&c<='9')a=a*10+c-48,c=getchar();return a*b;}
-
+ 
 int m,n;
 double area;
-
+ 
 struct Point 
 {
     double x,y;
@@ -17,7 +17,7 @@ Vector operator - (Point A,Point B) { return Vector(A.x-B.x,A.y-B.y); }
 Vector operator * (Vector A,double p) { return Vector(A.x*p,A.y*p); }
 inline double Cross(Vector A, Vector B) { return A.x*B.y - A.y*B.x; }
 double DBXMJ(Point p[], int n,double areaT=0){for(int i=1;i<n-1;i++) areaT+=Cross(p[i]-p[0],p[i+1]-p[0]);return fabs(areaT/2);}
-
+ 
 void Solve()
 {
     m=qrd();
@@ -27,10 +27,10 @@ void Solve()
         for(int j=0;j<n;j++) cin>>V[i][j].x>>V[i][j].y;
         area+=DBXMJ(V[i],n);
     }
-    printf("%.0lf\n",area);
+    printf("%lld\n",(long long)area);
     return;
 }
-
+ 
 int main()
 {
     int T=1;//qrd();
