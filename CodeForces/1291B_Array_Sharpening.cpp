78a1b0cab919a31a3ab5671a350a7d41de6ba271
Space <Spacelessd@outlook.com>
2020-10-05 10:49:10 +0800

    Auto Push
    Former-commit-id: 5ca0c98045cbefe1b0f6ad421f6c3da068b939cf

--- CodeForces/1291B_Array_Sharpening.cpp
+++ CodeForces/1291B_Array_Sharpening.cpp
@@ -5,16 +5,44 @@
 using namespace std;
 int qrd(){int a=0,b=1,c=getchar();while(c>'9'||c<'0'){if(c=='-')b=-1;c=getchar();}while(c>='0'&&c<='9')a=a*10+c-48,c=getchar();return a*b;}
 
-int n;
+int n,maxi;
+int a[300003];
+bool l[300003],r[300003];
 
 void Solve()
 {
     n=qrd();
+    for(int i=0;i<n;i++) a[i]=qrd(),l[i]=r[i]=false;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>=i)
+        {
+            l[i]=true;
+            if(i>0 && !l[i-1]) l[i]=false;
+        }
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+        if(a[i]>=n-1-i)
+        {
+            r[i]=true;
+            if(i<n-1 && !r[i+1]) r[i]=false;
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(l[i] && r[i])
+        {
+            printf("Yes\n");
+            return;
+        }
+    }
+    printf("No\n");
 }
 
 int main()
 {
-    int T=1;//qrd();
+    int T=qrd();
     while(T--) Solve();
     return 0;
 }
