d8fc3af1b47d9246102b5ff005fbb4e38f39d4a9
Space <Spacelessd@outlook.com>
2020-10-08 11:06:19 +0800

    Auto Push
    Former-commit-id: 4ff8edef6c3f5bd8d4d4f70e6104bac362ee12a4

--- ATCoder/ARC070F_HonestOrUnkind.cpp
+++ ATCoder/ARC070F_HonestOrUnkind.cpp
@@ -5,11 +5,39 @@
 using namespace std;
 int qrd(){int a=0,b=1,c=getchar();while(c>'9'||c<'0'){if(c=='-')b=-1;c=getchar();}while(c>='0'&&c<='9')a=a*10+c-48,c=getchar();return a*b;}
 
-int n;
+int a,b;
+int ans[4003];
+stack<int> Stack;
+
+bool query(int x, int y)
+{
+    printf("? %d %d\n", x, y);
+    fflush(stdout);
+    char s[3]; scanf("%s", s);
+    return s[0] == 'Y';
+}
 
 void Solve()
 {
-    n=qrd();
+    a=qrd();b=qrd();
+    if (a<=b) 
+    {
+        puts("Impossible");
+        return;
+    }
+    for (int i=0;i<a+b;i++)
+    {
+        if (Stack.empty()) Stack.push(i);
+        else
+        {
+            if (query(Stack.top(), i)) Stack.push(i);
+                else Stack.pop();
+        }
+    }
+
+    for (int i = 0; i < a + b; ++i) if (query(Stack.top(), i)) ans[i] = 1;
+    printf("! ");
+    for (int i = 0; i < a + b; ++i) printf("%d", ans[i]);
 }
 
 int main()
