e05e275d47b810533b0e9fc114d03e353c9da52d
Space <Spacelessd@outlook.com>
2020-10-27 17:19:06 +0800

    Auto Push
    Former-commit-id: d4c5a93c929a68cc6a70ef2fb81f11cb77656791

--- CodeForces/580C_Kefa_and_Park.cpp
+++ CodeForces/580C_Kefa_and_Park.cpp
@@ -13,8 +13,20 @@ int n,m,ans,tot;
 int a[100003],head[200010];
 struct{int nxt,to;}e[200010];
 
-void dfs(int x,int fa,int val){
-    
+void dfs(int x,int fa,int val,bool flag=true){
+    for(int i=head[x];i;i=e[i].nxt){
+        if(fa==e[i].to) continue;
+        flag=false;
+        if(a[e[i].to])dfs(e[i].to,x,a[e[i].to]+val);
+        else dfs(e[i].to,x,0);
+    }
+    if(flag) ans++;
+}
+
+void add(int x,int y){//x->y的边
+    e[++tot].to=y;//到y节点
+    e[tot].nxt=head[x];//当前的head[x]即为接在后面的边
+    head[x]=tot;//head 头部,tot指向第几个条边
 }
 
 void work()
@@ -22,9 +34,10 @@ void work()
     n=read();m=read();
     for(int i=1;i<=n;i++)a[i]=read();
     for(int u,v,i=1;i<n;i++){
-
+        u=read();v=read();
+        add(u,v);add(v,u);
     }
-    dfs(1,0,0);
+    dfs(1,0,a[1]);
     cout<<ans<<endl;
     return; 
 }
---
+++ CodeForces/580C_LGstd.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define LL long long
+#define inf LONG_LONG_MAX
+#define he(x) for(register LL i=head[x];i;i=e[i].nxt)
+using namespace std;
+LL n,m,ans,tot,head[200010],val[100010];
+struct CCF{
+	LL nxt,to;
+}e[200010];
+
+inline void add(LL x,LL y){
+	e[++tot].to=y;
+	e[tot].nxt=head[x];
+	head[x]=tot;
+}
+inline void dfs(LL x,LL fa,LL w){
+	bool flag=false;
+	he(x){
+		LL y=e[i].to;
+		if(y==fa)continue;
+		flag=true;
+        //如果它有儿子，就不是叶子节点
+		if(val[y]+w>m)continue;
+  		//超过m，不做了
+		if(val[y])dfs(y,x,val[y]+w);
+		else dfs(y,x,0);
+  		//点权为0，连续点权清零
+	}
+	if(!flag)ans++;
+}
+int main()
+{
+	cin>>n>>m;
+	for(int i=1;i<=n;i++)read(val[i]);
+	for(int i=1;i<n;i++){
+		LL u,v;
+		cin>>u>>v;
+		add(u,v);add(v,u);
+	}
+	dfs(1,0,val[1]);
+	cout<<ans<<endl;
+    return 0;
+}
