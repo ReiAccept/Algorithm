6ae15c4d1fed48821e7224022ce5cedce2fd8da3
Space <i@spaceless.me>
2020-11-01 22:40:48 +0800

    Auto Push
    Former-commit-id: 7536c0784f81f60bc651050ca7aa7a1076832f6e

--- Template/DefaultCode.cpp
+++ Template/DefaultCode.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#define int long long
 #define ll long long
 #define ull unsigned long long
 
@@ -7,17 +6,8 @@ using namespace std;
 
 int read(){char c;int num,f=1;while(c=(char)getchar(),!isdigit(c))if(c=='-')f=-1;num=(int)(c-'0');while(c=(char)getchar(),isdigit(c))num=num*10+(int)(c-'0');return num*f;}
 
-int n,m,q,p,k;
-int x[1000003];
-
 void work()
 {
-    n=read();m=read();q=read();
-    for(int i=1;i<=n;i++)x[i]=read();
-    while(q--)
-    {
-        p=read();k=read();
-    }
     return;
 }
 
