1cd4888f8ee5e4085f5c18953dc0466265a6f85a
Space <i@spaceless.me>
2021-01-08 02:20:47 +0800

    Quick Push
    Former-commit-id: f9e7e1c3c2c9bb9bb276535cb0c3c096e34f2e73

--- HDUOJ/ACM_Steps/Chapter_Two/Section_Two/2.cpp
+++ HDUOJ/ACM_Steps/Chapter_Two/Section_Two/2.cpp
@@ -13,8 +13,39 @@ const int INF=0x3f3f3f3f;
 int read(){char c;int num,f=1;while(c=(char)getchar(),!isdigit(c))if(c=='-')f=-1;num=(int)(c-'0');while(c=(char)getchar(),isdigit(c))num=num*10+(int)(c-'0');return num*f;}
 void prt(int x){if(x<0){putchar('-');x=-x;}if(x>9)prt(x/10);putchar((char)(x%10+'0'));}
 
+//int a[15]={0,2,7,5,30,169,441,1872,7632,1740,93313,459901,1358657,2504881};//其实可以打表过
+int n;
+int vis[16];
+
 void work()
 {
+    while(n=read())
+    {
+        if(vis[n])
+        {
+            cout<<vis[n]<<endl;
+            continue;
+        }
+        for(int ans=n+1;!vis[n];ans++)
+        {
+            int sum=n*2;
+            for(int j=ans;;j+=ans-1)
+            {
+                if(j>sum)
+                {
+                    j= j%sum ? j%sum : sum;
+                }
+                if(j<=n) break;
+                else sum--;
+                if(sum==n)
+                {
+                    vis[n]=ans;
+                    cout<<vis[n]<<endl;
+                    break;
+                }
+            }
+        }
+    }
     return;
 }
 
