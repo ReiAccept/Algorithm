2bcc837de718b3385611ffdf1cdfb02e9f8b82ab
Space <i@spaceless.me>
2021-05-03 17:49:26 +0800

    Quick Push
    Former-commit-id: 12878e1f8566bc663d60377591aee5febd48282f

---
+++ CodeForces/1517/A.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define int long long//__int128
+#define mmst0(x) memset(x,0,sizeof(x))
+#define mmst3f(x) memset(x,0x3f,sizeof(x))
+#define pb(x) emplace_back(x)
+#define mkp(x, y) make_pair(x, y)
+#define fi first
+#define se second
+using namespace std;
+typedef long long ll;
+typedef long double rld;
+typedef unsigned long long ull;
+
+const rld eps = 1e-6;
+const int INF=0x3f3f3f3f;//0x3f3f3f3f3f3f3f3f;//LLINF
+
+int read(){int s=0,w=1;char ch=getchar();while(!isdigit(ch)){if(ch=='-')w=-1;ch=getchar();}while(isdigit(ch)){s=(s<<3)+(s<<1)+(ch^48);ch=getchar();} return s*w;}
+//void prt(int x){if(x<0){putchar('-');x=-x;}if(x>9)prt(x/10);putchar((char)(x%10+'0'));}
+
+int n;
+
+void work()
+{
+    n=read();
+    int d=2050*(10e13),cnt=0;
+    while(n)
+    {
+        if(n<2050 && n!=0)
+        {
+            printf("-1\n");
+            return;
+        }
+        while(d>n) d/=10;
+        n-=d;cnt++; 
+    }
+    printf("%lld\n",cnt);
+    return;
+}
+
+signed main()
+{
+    //ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); //freopen(".in", "r", stdin);//freopen(".out", "w", stdout);
+    signed T=read();
+    for(signed Case=1; Case<=T; Case++)
+    {
+        //printf("Case %d: ",Case);
+        work();
+    }
+    return 0;
+}
+
