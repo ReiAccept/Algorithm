20ba0a7a0c2352efa72ff1a2e5d68e4e3b22411b
Rei <Spacelessd@outlook.com>
2021-09-08 16:01:03 +0800

    Auto Push

--- CFGym/102992_2020-2021_ACM-ICPC_Asia_Nanjing_Regional_Contest/LR.cpp
+++ CFGym/102992_2020-2021_ACM-ICPC_Asia_Nanjing_Regional_Contest/LR.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 //#include<bits/extc++.h>
-//#define int long long//__int128
+#define int long long//__int128
 #define mmst0(x) memset(x,0,sizeof(x))
 #define mmst3f(x) memset(x,0x3f,sizeof(x))
 #define pb(x) emplace_back(x)
@@ -22,26 +22,30 @@ inline int read(){int s=0,w=1;char ch=nc();while(!isdigit(ch)){if(ch=='-')w=-1;c
 //inline void read(int &x){char ch=nc();x=0;while (!(ch>='0'&&ch<='9')) ch=nc();while (ch>='0'&&ch<='9') x=(x<<3)+(x<<1)+ch-48,ch=nc();}//根据参数个数自动选择
 //void prt(int x){if(x<0){putchar('-');x=-x;}if(x>9)prt(x/10);putchar((char)(x%10+'0'));}
 
-int n,m;
+int n,m,ans;
 int a[MAXN],b[MAXN];
 
 inline void work()
 {
-    n=read();m=read(); b[0]=-INF,b[m+1]=INF;
+    n=read();m=read(); b[0]=-INF,b[m+1]=INF; ans=0;
     for(int i=1;i<=n;i++) a[i]=read();
     for(int i=1;i<=m;i++) b[i]=read();
-    sort(a+1,a+1+n); sort(b+1,b+1+1);
+    sort(a+1,a+1+n); sort(b+1,b+1+m);
     for(int i=1;i<=m+1;i++)
     {
-
+        int lf=upper_bound(a+1,a+1+n,b[i-1])-a;
+        int rt=lower_bound(a+1,a+1+n,b[i])-a-1;
+        ans=max(ans,rt-lf+1);
     }
+    if(ans<=0) printf("Impossible\n");
+    else printf("%lld\n",ans);
     return;
 }
 
 signed main()
 {
     //ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); //freopen(".in", "r", stdin);//freopen(".out", "w", stdout);
-    signed T=1;//(signed)read();//scanf("%d",&T);//cin>>T;
+    signed T=(signed)read();//scanf("%d",&T);//cin>>T;
     for(signed Case=1; Case<=T; Case++)
     {
         //printf("Case %d: ",Case);
@@ -50,3 +54,21 @@ signed main()
     }
     return 0;
 }
+/*
+3
+
+2 2
+2 3
+1 4
+-> 1,(2,3),4
+
+6 5
+1 2 3 5 7 7
+1 3 3 4 10
+-> 4,(5,7,7),10
+
+1 1
+7
+7
+-> Impossible
+*/
