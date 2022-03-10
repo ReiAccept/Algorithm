24dabbaca9e60c1937095cc96ce3e78d70d10cc0
ACRei <lhh@live.com>
2022-03-10 17:59:51 +0800

    Daily Push

--- DmyOJ/p469.cpp
+++ DmyOJ/p469.cpp
@@ -5,7 +5,6 @@
 #define mmst3f(x) memset(x,0x3f,sizeof(x))
 #define si(x) scanf("%d",&x)//scanf("%lld",&x) // When define int ll
 #define pb(x) emplace_back(x)
-#define sz(x) int(x.size())
 #define PII pair<int,int>
 #define mkp(x, y) make_pair(x, y)
 #define fi first
@@ -20,7 +19,7 @@ typedef unsigned long long ull;
 
 const double eps = 1e-6;
 const int INF=0x3f3f3f3f;//0x3f3f3f3f3f3f3f3f; // LLINF
-const int MAXN=(int)1e5+3;
+const int MAXN=(int)5e5+3;
 
 inline char nc(){static char buf[100000],*p1=buf,*p2=buf;return p1==p2&&(p2=(p1=buf)+fread(buf,1,100000,stdin),p1==p2)?EOF:*p1++;}
 inline int read(){int s=0,w=1;char ch=nc();while(!isdigit(ch)){if(ch=='-')w=-1;ch=nc();}while(isdigit(ch)){s=(s<<3)+(s<<1)+(ch^48);ch=nc();} return s*w;}
@@ -28,7 +27,43 @@ inline int read(){int s=0,w=1;char ch=nc();while(!isdigit(ch)){if(ch=='-')w=-1;c
 // inline void read(int &x){char ch=nc();x=0;while (!(ch>='0'&&ch<='9')) ch=nc();while (ch>='0'&&ch<='9') x=(x<<3)+(x<<1)+ch-48,ch=nc();} // 根据参数个数自动选择
 // void prt(int x){if(x<0){putchar('-');x=-x;}if(x>9)prt(x/10);putchar((char)(x%10+'0'));}
 
+int n,m;
+int a[MAXN],l[MAXN],r[MAXN],ans[MAXN],ord[MAXN],tr[MAXN];
+map<int,int> mp;
+
 inline void work(signed CASE=1,bool FINAL_CASE=false) {
+    // int j,k,t;
+	memset(tr,3,sizeof(tr));
+	n=read(); m=read();
+	for(int i=1;i<=n;i++) {
+        a[i]=read();
+    }
+	for(int i=0;i<m;i++) {
+        ord[i]=i;
+        l[i]=read(); r[i]=read();
+    }
+    sort(ord,ord+m,[](const int &xx,const int &yy) {
+        return r[xx]<r[yy];
+    });
+	for(int i=0,j=1;i<m;i++) {
+		int t=ord[i];
+		while(j<=r[t]) {
+			if(mp.count(a[j])) {
+                for(int k=mp[a[j]];k;k-=k&-k) {
+                    tr[k]=min(tr[k],j-mp[a[j]]);
+                }
+            }
+			mp[a[j]]=j;
+			j++;
+		}
+        ans[t]=1e6;
+		for(int k=l[t];k<=n;k+=k&-k) {
+            ans[t]=min(ans[t],tr[k]);
+        }
+	}
+	for(int i=0;i<m;i++) {
+        printf("%d\n",ans[i]==1e6?-1:ans[i]);
+    }
     return;
 }
 
--- DmyOJ/p497.cpp
+++ DmyOJ/p497.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 // #include<bits/extc++.h>
-// #define int long long//__int128
+#define int long long//__int128
 #define mmst0(x) memset(x,0,sizeof(x))
 #define mmst3f(x) memset(x,0x3f,sizeof(x))
 #define si(x) scanf("%d",&x)//scanf("%lld",&x) // When define int ll
 #define pb(x) emplace_back(x)
-#define sz(x) int(x.size())
+#define sz(x) ((int)x.size())
 #define PII pair<int,int>
 #define mkp(x, y) make_pair(x, y)
 #define fi first
@@ -20,7 +20,7 @@ typedef unsigned long long ull;
 
 const double eps = 1e-6;
 const int INF=0x3f3f3f3f;//0x3f3f3f3f3f3f3f3f; // LLINF
-const int MAXN=(int)1e5+3;
+const int MAXN=(int)3e5+3;
 
 inline char nc(){static char buf[100000],*p1=buf,*p2=buf;return p1==p2&&(p2=(p1=buf)+fread(buf,1,100000,stdin),p1==p2)?EOF:*p1++;}
 inline int read(){int s=0,w=1;char ch=nc();while(!isdigit(ch)){if(ch=='-')w=-1;ch=nc();}while(isdigit(ch)){s=(s<<3)+(s<<1)+(ch^48);ch=nc();} return s*w;}
@@ -28,7 +28,43 @@ inline int read(){int s=0,w=1;char ch=nc();while(!isdigit(ch)){if(ch=='-')w=-1;c
 // inline void read(int &x){char ch=nc();x=0;while (!(ch>='0'&&ch<='9')) ch=nc();while (ch>='0'&&ch<='9') x=(x<<3)+(x<<1)+ch-48,ch=nc();} // 根据参数个数自动选择
 // void prt(int x){if(x<0){putchar('-');x=-x;}if(x>9)prt(x/10);putchar((char)(x%10+'0'));}
 
+int n,ans,x;
+int a[MAXN],st[MAXN];
+int cnt[40][2];
+vector<int> vec;
+
+void solve(vector<int> vec,int p) {
+	if(p==-1||vec.empty()) {
+        return;
+    }
+	vector<int> tmp[2];
+	for(auto x:vec) {
+        tmp[a[x]>>p&1].pb(x);
+    }
+	ll sum=0;
+	for(int i=0,j=0;i<sz(tmp[0]);i++) {
+		while(j<sz(tmp[1])&&tmp[0][i]>tmp[1][j]) ++j;
+		sum+=j;
+	}
+	cnt[p][0]+=sum;
+	cnt[p][1]+=1ll*sz(tmp[0])*sz(tmp[1])-sum;
+	solve(tmp[0],p-1); solve(tmp[1],p-1);
+}	
+
 inline void work(signed CASE=1,bool FINAL_CASE=false) {
+    n=read();
+	for(int i=1;i<=n;i++) {
+        a[i]=read();
+    }
+	for(int i=1;i<=n;i++) {
+        vec.pb(i);
+    }
+	solve(vec,30);
+	for(int i=30;~i;i--) {
+		ans+=min(cnt[i][0],cnt[i][1]);
+		if(cnt[i][1]<cnt[i][0]) x|=1<<i;
+	}
+    printf("%lld %lld\n",ans,x);
     return;
 }
 
